c41e12d1606ee192aeeb4183a96f68e5e897eb48
Sam Atman <atmanistan@gmail.com>
2024-01-03 11:26:07 -0500

    Add types.jl

---
+++ src/types.jl
@@ -0,0 +1,56 @@
+
+"""
+    Instruction
+
+Abstract type of JLPeg VM instructions.
+
+| Field | Meaning                                            |
+|-------|----------------------------------------------------|
+| op    | The Opcode                                         |
+| l     | An instruction offset                              |
+| n     | A subject offset                                   |
+| tag   | Key in a capture/throw Dict, or mark identity      |
+| c     | An AbstractChar to match                           |
+| vec   | A set's test BitVector                             |
+| lead  | Lead bytes of a MultiSet                           |
+| final | Is this Set the final in the sequence              |
+
+"""
+abstract type Instruction end
+
+"""
+Container for various patterns and grammars.
+Always has `val`, which may be primitive or a PVector,
+and `code`, an IVector. Some patterns have a field
+unique to that pattern type.
+"""
+abstract type Pattern <: AbstractPattern end
+
+"""
+    PegError(msg)
+
+An error while constructing a JLPeg Pattern.
+"""
+struct PegError <: Exception
+    msg::String
+end
+
+
+"""
+    Instruction
+
+Abstract type of JLPeg VM instructions.
+
+| Field | Meaning                                            |
+|-------|----------------------------------------------------|
+| op    | The Opcode                                         |
+| l     | An instruction offset                              |
+| n     | A subject offset                                   |
+| tag   | Key in a capture/throw Dict, or mark identity      |
+| c     | An AbstractChar to match                           |
+| vec   | A set's test BitVector                             |
+| lead  | Lead bytes of a MultiSet                           |
+| final | Is this Set the final in the sequence              |
+
+"""
+abstract type Instruction end
