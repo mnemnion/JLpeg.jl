0504d1190f45e66031acc4d9d7f7e58e5a21b341
Sam Atman <atmanistan@gmail.com>
2024-02-10 19:03:17 -0500

    More benchmarks

--- benchmark/bench_kjv.jl
+++ benchmark/bench_kjv.jl
@@ -5,8 +5,12 @@ kjv = read("kingjames.txt", String)
 
 trials = BenchmarkGroup()
 
-people = ((P"Abraham",) | (P"Sarah",) | (P"Moses",) | (P"Matthew",) | (P"Tubalcain",) | P(1))^1
-kjv_end = P"" >> P"www.gutenberg.org."
+people = compile!(((P"Abraham",) | (P"Sarah",) | (P"Moses",) | (P"Matthew",) | (P"Tubalcain",) | P(1))^1)
+kjv_end = compile!(P"" >> P"www.gutenberg.org.")
+fourteen = compile!(P"" >> ((R"az" | R"AZ")^[14],))
 
 trials["people"] = @benchmarkable match($people, $kjv)
 trials["to_end"] = @benchmarkable match($kjv_end, $kjv)
+trials["fourteen"] = @benchmarkable match($fourteen, $kjv)
+
+tune!(trials)
