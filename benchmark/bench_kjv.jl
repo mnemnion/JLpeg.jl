e0da184f843a2a60c19be0f652bdfc2f4cefa67e
Sam Atman <atmanistan@gmail.com>
2024-02-14 18:17:12 -0500

    Add brackets benchmark

--- benchmark/bench_kjv.jl
+++ benchmark/bench_kjv.jl
@@ -3,14 +3,26 @@ using JLpeg
 
 kjv = read("kingjames.txt", String)
 
-trials = BenchmarkGroup()
+suite = BenchmarkGroup()
 
-people = compile!(((P"Abraham",) | (P"Sarah",) | (P"Moses",) | (P"Matthew",) | (P"Tubalcain",) | P(1))^1)
-kjv_end = compile!(P"" >> P"www.gutenberg.org.")
-fourteen = compile!(P"" >> ((R"az" | R"AZ")^[14],))
+const people = compile!(((P"Abraham",) | (P"Sarah",) | (P"Moses",) | (P"Matthew",) | (P"Tubalcain",) | P(1))^1)
+const kjv_end = compile!(P"" >> P"www.gutenberg.org.")
+const fourteen = compile!(P"" >> ((R"az" | R"AZ")^[14],))
 
-trials["people"] = @benchmarkable match($people, $kjv)
-trials["to_end"] = @benchmarkable match($kjv_end, $kjv)
-trials["fourteen"] = @benchmarkable match($fourteen, $kjv)
+suite["people"] = @benchmarkable match($people, $kjv)
+suite["to_end"] = @benchmarkable match($kjv_end, $kjv)
+suite["fourteen"] = @benchmarkable match($fourteen, $kjv)
 
-tune!(trials)
+# Might split this into its own file
+
+@grammar brackets begin
+    :brackets ← "[" * :brackets^0 * "]"
+ end
+
+compile!(brackets)
+
+const longbracket = "["^1000000 * "]"^1000000
+
+suite["brace_match"] = @benchmarkable match($brackets, $longbracket)
+
+tune!(suite)
